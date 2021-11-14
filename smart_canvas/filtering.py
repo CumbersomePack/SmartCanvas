db6974d3e25ca5cb99babe5fd0603c58c8cc277d
jvuorine <vuorinen.jere@gmail.com>
2021-11-14 11:54:08 +0200

    remove unnecessary lines, add comments

--- smart_canvas/filtering.py
+++ smart_canvas/filtering.py
@@ -10,6 +10,26 @@ from numpy.lib.function_base import gradient
 
 # Internal packages
 
+def makeStroke(brush, x, y, img):
+    '''
+    add diagonal stroke to list and return for the paintLayer function
+    '''
+    if x > img.shape[1]-1:
+        x = img.shape[1]-1
+    if y > img.shape[0]-1:
+        y = img.shape[0]-1
+    stroke_color = img[y,x]
+    stroke_len = np.random.randint(1,8)
+    x_0 = x - stroke_len
+    y_0 = y - stroke_len
+    if x_0 < 0:
+        x_0 = 0
+    if y_0 < 0:
+        y_0 = 0
+    stroke = [(x_0,y_0), (x_0 + stroke_len,y_0 + stroke_len), stroke_color.tolist(), brush]
+
+    return stroke
+
 def makeSplineStroke(x, y, brush, img, canvas, gradients):
     '''
     adds a brush stroke to a list and return it for the paintLayer function to paint
@@ -19,8 +39,9 @@ def makeSplineStroke(x, y, brush, img, canvas, gradients):
     stroke_points = [[x, y]]
     lastDx, lastDy = (0,0)
 
-    maxStrokeLength = 20
-    minStrokeLength = 4
+    maxStrokeLength = 16
+    minStrokeLength = 6
+    # f_c controls the usage of the previous gradient vs the current gradient
     f_c = 1.0
 
     for i in range(maxStrokeLength):
@@ -57,10 +78,9 @@ def paintLayer(canvas, ref_img, brush, gradients):
     '''
     strokes = []
 
+    # f_g is the parameter to control grid size as a multiple of brush size
     f_g = 1.5
     grid = int(f_g * brush)
-
-    threshold = 50
 
     for x in range(0, ref_img.shape[1], grid):
         for y in range(0, ref_img.shape[0], grid):
@@ -83,8 +103,12 @@ def paint(image):
     canvas = np.zeros((height, width,3), dtype=np.uint8)
     gradients = calcImageGradients(image)
 
+    # f_s controls the blur amount of the reference image
     f_s = 1.0
-    brush_sizes = [8,6,3]
+    # small brush sizes are slow to compute
+    # large brushes are not precise enough
+    # TODO: find a solution for this
+    brush_sizes = [8,6,4]
 
     for brush in brush_sizes:
         ref_img = cv2.GaussianBlur(image, (0,0), sigmaX=f_s*brush, sigmaY=f_s*brush)
@@ -102,6 +126,8 @@ def calcImageGradients(img):
     img = cv2.GaussianBlur(img, (3,3), sigmaX=1, sigmaY=1)
     grad_x = cv2.Sobel(img, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=5)
     grad_y = cv2.Sobel(img, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=5)
+    
+    # code breaks if we don't include these, "index -1072 is out of bounds for axis 1 with size 490" at "if g_mag[y,x] == 0:" in makeSplineStroke()
     grad_x = grad_x / np.max(grad_x)
     grad_y = grad_y / np.max(grad_y)
     grad_magnitude = np.sqrt((grad_x ** 2) + (grad_y ** 2))
